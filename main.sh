6f82b6e4cc21fe1cb314588748e5d8e4f2b65c81
ProExecutor <123886692+ProExecutor@users.noreply.github.com>
2023-12-21 02:35:43 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,5 +2,5 @@ termux-setup-storage
 apt update
 apt upgrade
 pkg install x11-repo
-pkg install qemu-system-x86_64
-qemu-system-x86_64 -m 1024M -hda /sdcard/Download/11.vhd -vga std -cpu core2duo -smp sockets=1,cores=4,threads=1 -device e1000,netdev=n0 -netdev user,id=n0 -vnc 127.0.0.1:2
+pkg install qemu-system-aarch64
+qemu-system-aarch64 -m 1024M -hda /sdcard/Download/11.vhd -vga std -cpu cortex-a57 -smp sockets=1,cores=4,threads=1 -device e1000,netdev=n0 -netdev user,id=n0 -vnc 127.0.0.1:2
